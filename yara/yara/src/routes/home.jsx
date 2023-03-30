71122124efa2bfa4feb87018fd80de72dc9495cb
Gabriel da Silva <115048603+bielkh@users.noreply.github.com>
2023-03-30 09:15:37 -0300

    Add files via upload

---
+++ yara/yara/src/routes/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/home.css';
+
+
+function Home() {
+    return (  
+        <div className="homeMain">
+
+            
+        </div>
+
+    );
+}
+
+export default Home;
