199a988754cd5fc87af2433e3076ada718bbcffa
Gabriel da Silva <115048603+bielkh@users.noreply.github.com>
2023-03-30 09:19:39 -0300

    Add files via upload

---
+++ yara/src/components/body.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/body.css'
+
+
+function Body() {
+    return (  
+        <div className="body-div-home">
+
+            
+        </div>
+
+    );
+}
+
+export default Body;
